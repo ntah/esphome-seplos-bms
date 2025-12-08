d2492591fbc11523d024df0d055b7b01036c7985
hmmm <fajrie@gmail.com>
2025-12-08 11:33:54 +0700

    add zte telemetry

--- components/seplos_bms/seplos_bms.h
+++ components/seplos_bms/seplos_bms.h
@@ -117,6 +117,8 @@ class SeplosBms : public PollingComponent, public seplos_modbus::SeplosModbusDev
   void publish_state_(sensor::Sensor *sensor, float value);
   void publish_state_(text_sensor::TextSensor *text_sensor, const std::string &state);
   void on_telemetry_data_(const std::vector<uint8_t> &data);
+  // ADD THIS for ZTE support:
+  void on_zte_telemetry_(const std::vector<uint8_t> &data);
   void reset_online_status_tracker_();
   void track_online_status_();
   void publish_device_unavailable_();
