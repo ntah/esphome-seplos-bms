de737e8f7e648ad0cd69775dbc832cf9bbddef7f
hmmm <fajrie@gmail.com>
2025-12-11 12:34:49 +0700

    Update seplos_modbus.h

--- components/seplos_modbus/seplos_modbus.h
+++ components/seplos_modbus/seplos_modbus.h
@@ -2,30 +2,61 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
-#include <vector>
-#include <string>
 
 namespace esphome {
 namespace seplos_modbus {
 
+class SeplosModbusDevice;
+
 class SeplosModbus : public uart::UARTDevice, public Component {
  public:
+  SeplosModbus() = default;
+
   void setup() override;
+
   void loop() override;
 
-  void send_ascii(uint8_t protocol, uint8_t address, uint8_t function, const std::string &payload);
+  void dump_config() override;
+
+  void register_device(SeplosModbusDevice *device) { this->devices_.push_back(device); }
+
+  float get_setup_priority() const override;
+
+  void send(uint8_t protocol_version, uint8_t address, uint8_t function, uint8_t value);
+  void set_rx_timeout(uint16_t rx_timeout) { rx_timeout_ = rx_timeout; }
+  void set_flow_control_pin(GPIOPin *flow_control_pin) { this->flow_control_pin_ = flow_control_pin; }
 
  protected:
-  bool parse_byte_(uint8_t data);
-  bool parse_frame_(const std::string &frame);
+  uint16_t rx_timeout_{150};
+  GPIOPin *flow_control_pin_{nullptr};
 
-  // CRC ASCII used by ZTE (CRC16 IBM)
-  uint16_t crc16_ascii_(const std::string &ascii);
+  bool parse_seplos_modbus_byte_(uint8_t byte);
+  std::vector<uint8_t> rx_buffer_;
+  uint32_t last_seplos_modbus_byte_{0};
+  uint32_t last_send_{0};
+  std::vector<SeplosModbusDevice *> devices_;
+};
 
-  void handle_packet_(uint8_t proto, uint8_t addr, uint8_t func, const std::string &frame);
+uint16_t crc16(const uint8_t *data, uint8_t len);
 
-  std::string rx_buffer_;
-  bool in_frame_ = false;
+class SeplosModbusDevice {
+ public:
+  void set_parent(SeplosModbus *parent) { parent_ = parent; }
+  void set_address(uint8_t address) { address_ = address; }
+  void set_pack(uint8_t pack) { pack_ = pack; }
+  void set_protocol_version(uint8_t protocol_version) { protocol_version_ = protocol_version; }
+  virtual void on_seplos_modbus_data(const std::vector<uint8_t> &data) = 0;
+  void send(uint8_t function, uint8_t value) {
+    this->parent_->send(this->protocol_version_, this->address_, function, value);
+  }
+
+ protected:
+  friend SeplosModbus;
+
+  SeplosModbus *parent_;
+  uint8_t address_;
+  uint8_t pack_;
+  uint8_t protocol_version_;
 };
 
 }  // namespace seplos_modbus
